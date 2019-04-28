885eab812eaa7489c8a265c060567f54b2a80f85
Chiqui34 <joseldd@gmail.com>
2019-04-28 08:52:04 +0200

    Sin terminar y con las líneas de código comentadas por las imágenes

---
+++ main.py
@@ -0,0 +1,45 @@
+import pygame
+import sys
+
+class Game():
+    
+    corredores = []
+    
+    def __init__(self):
+        
+        self.__screen = pygame.display.set_mode((640, 480))
+        pygame.display.set_caption("Carrea de bichos")
+        #self.background = pygame.image.load("images/background.png")
+        
+        #self.runner = pygame.image.load("images/smallball.png")
+        
+        def competir(self):
+            
+            x = 0
+            hayGanador = False
+            
+            while not hayGanador:
+                # Comprobación de los eventos
+                for event in paygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+                
+                #Refrescar / renderizar la pantalla
+                self.__screen.blit(self.background. (0, 0))
+                self.__screen.blit(self.runner, (x, 240))
+                pygame.display.flip()
+                
+                x += 3
+                if x >= 250:
+                    hayGanador = True
+                
+                
+            pygame.quit()
+            sys.quit()
+                                   
+        
+if __name__ == '__main__':
+    pygame.init()
+    game = Game()
+    game.competir()
